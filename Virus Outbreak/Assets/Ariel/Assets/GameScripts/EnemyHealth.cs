130aa6202577d25b8d376895362a154981ddd014
Ariel <005607559@coyote.csusb.edu>
2018-11-20 19:02:39 -0800

    Ariel added his stuff

---
+++ Virus Outbreak/Assets/Ariel/Assets/GameScripts/EnemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int health;
+
+    //private int addScore = 50;
+
+    private void OnCollisionEnter2D(Collision2D player) //Placeholder until I add bullets
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            Debug.Log("Collided with the Player");
+            health -= 1;
+
+            if (health == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
